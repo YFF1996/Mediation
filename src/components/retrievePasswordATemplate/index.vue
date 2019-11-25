c3eb21596a3222eff4f8df4bcf10dca61c6d2c9d
YFF <yff@YFFdeMacBook-Pro.local>
2019-11-25 23:43:05 +0800

    添加修改密码页面

---
+++ src/components/retrievePasswordATemplate/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="retrieve-password-a"></div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {}
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
